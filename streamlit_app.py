6a32e014f6bfc2d394056ed18b6a5ad641b0efeb
Rihantech5 <rihantech5@gmail.com>
2025-10-27 23:13:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
 st.title("Matematika Pak Rifqi")
-st.write(
-    "Ayo belajar matematika bareng Pak Rifqi")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-st.image("https://www.google.com/imgres?q=math&imgurl=https%3A%2F%2Fwww.susla.edu%2Fassets%2Fsusla%2Fimages%2FMathmaticsImage.jpeg&imgrefurl=https%3A%2F%2Fwww.susla.edu%2Fpage%2Fmathematics&docid=RaNMEeeKxDMrCM&tbnid=UEhlQKQll0kHZM&vet=12ahUKEwjQwMbD38SQAxUHRWcHHWkbIz4QM3oECBsQAA..i&w=3000&h=2000&hcb=2&ved=2ahUKEwjQwMbD38SQAxUHRWcHHWkbIz4QM3oECBsQAA", caption="Sunrise by the mountains")
+st.write("Ayo belajar matematika bareng Pak Rifqi")
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
