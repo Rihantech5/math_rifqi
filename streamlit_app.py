87f0ce7f37e68404a04b11b30b2b22abf0e0af77
Rihantech5 <rihantech5@gmail.com>
2025-10-27 23:00:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-st.image("sunrise.jpg", caption="Sunrise by the mountains")
+st.image("https://www.google.com/imgres?q=math&imgurl=https%3A%2F%2Fwww.susla.edu%2Fassets%2Fsusla%2Fimages%2FMathmaticsImage.jpeg&imgrefurl=https%3A%2F%2Fwww.susla.edu%2Fpage%2Fmathematics&docid=RaNMEeeKxDMrCM&tbnid=UEhlQKQll0kHZM&vet=12ahUKEwjQwMbD38SQAxUHRWcHHWkbIz4QM3oECBsQAA..i&w=3000&h=2000&hcb=2&ved=2ahUKEwjQwMbD38SQAxUHRWcHHWkbIz4QM3oECBsQAA", caption="Sunrise by the mountains")
