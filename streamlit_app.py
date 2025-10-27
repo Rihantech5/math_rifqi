e494830c6a57ef4aea3b2a9cfb9cf3f65d7af79c
Rihantech5 <rihantech5@gmail.com>
2025-10-27 22:37:08 +0700

    Fix string formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Matematika Pak Rifqi")
 st.write(
-    "Ayo belajar matematika bareng Pak Rifqi"
+    \n"Ayo belajar matematika bareng Pak Rifqi"
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
